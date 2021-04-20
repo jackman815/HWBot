cefbf00b35122cf1dd592f54c7c45c2de8095b7c
Jack Man <29057227+jackman815@users.noreply.github.com>
2021-04-20 13:47:58 +0800

    commit

--- main.py
+++ main.py
@@ -21,16 +21,13 @@ for course in courses:
             if (len(activity.find_elements_by_class_name('contentwithoutlink')) > 0):
                 continue
             current_activity = activity.find_element_by_css_selector('.activityinstance')
-            if (len(activity.find_elements_by_class_name('contentwithoutlink')) > 0):
-                activity_list.append([current_activity.find_element_by_css_selector('.instancename').text, ''])
-            else:
-                activity_list.append([current_activity.find_element_by_css_selector('.instancename').text,
-                                      current_activity.find_element_by_css_selector('.activityinstance>a').get_attribute('href')])
+            activity_list.append([current_activity.find_element_by_css_selector('.instancename').text,
+                                  current_activity.find_element_by_css_selector('.activityinstance>a').get_attribute('href')])
 
         activitys.append([topic.find_element_by_css_selector('.sectionname>span>a').get_attribute('innerHTML'),
                           activity_list])
     data.append([course[0], activitys])
-    browser.implicitly_wait(1)
+    #browser.implicitly_wait(1)
 print(data)
 browser.quit()
 
