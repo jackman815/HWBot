ea69f4f51249a184235b69f0ede4f43940a85a29
Jack Man <29057227+jackman815@users.noreply.github.com>
2021-04-28 21:43:43 +0800

    Fix data = 0

--- discord_handler.py
+++ discord_handler.py
@@ -20,15 +20,18 @@ async def ping(ctx):
 async def due(ctx):
     await ctx.reply(embed=discord.Embed(title="Fetching Data....", color=0x00ff59))
     data = grab_data()
-    for i in data:
-        embed = discord.Embed(title=(i['name']).replace(" is due", ""),
-                              url=html.unescape(i['action']['url']),
-                              description=i['description'], color=0x00ff59)
-        embed.set_author(name=i['course']['fullname'],
-                         url=i['course']['viewurl'])
-        embed.add_field(name="Due date",
-                        value=datetime.fromtimestamp(i['timesort'], tz=timezone(timedelta(hours=8))).strftime(
-                            '%Y-%m-%d %H:%M:%S'), inline=True)
-        embed.add_field(name="Days remaining", value=str((datetime.fromtimestamp(i['timesort'], tz=timezone(
-            timedelta(hours=8))) - datetime.now(timezone(timedelta(hours=8)))).days), inline=True)
-        await ctx.send(embed=embed)
+    if len(data) > 0:
+        for i in data:
+            embed = discord.Embed(title=(i['name']).replace(" is due", ""),
+                                  url=html.unescape(i['action']['url']),
+                                  description=i['description'], color=0x00ff59)
+            embed.set_author(name=i['course']['fullname'],
+                             url=i['course']['viewurl'])
+            embed.add_field(name="Due date",
+                            value=datetime.fromtimestamp(i['timesort'], tz=timezone(timedelta(hours=8))).strftime(
+                                '%Y-%m-%d %H:%M:%S'), inline=True)
+            embed.add_field(name="Days remaining", value=str((datetime.fromtimestamp(i['timesort'], tz=timezone(
+                timedelta(hours=8))) - datetime.now(timezone(timedelta(hours=8)))).days), inline=True)
+            await ctx.send(embed=embed)
+    else:
+        await ctx.reply(embed=discord.Embed(title="No recent events.", color=0x00ff59))
