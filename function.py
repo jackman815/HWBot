8bb24071bc90c23e393a3500c05dac2d120c2868
Jack Man <29057227+jackman815@users.noreply.github.com>
2021-04-28 13:04:07 +0800

    commit

--- function.py
+++ function.py
@@ -1,5 +1,4 @@
 import os
-
 from selenium import webdriver
 import json
 import requests
@@ -13,6 +12,30 @@ def grab_courses_list(mycourses):
         courses.append([course.find_element_by_tag_name('a').get_attribute('innerHTML'),
                         course.find_element_by_tag_name('a').get_attribute('href')])
     return courses
+
+
+def fetch_all_assin(browser):
+    mycourses = browser.find_element_by_class_name('mycourses').find_elements_by_tag_name('li')
+    courses = grab_courses_list(mycourses)
+    data = []
+
+    for course in courses:
+        activitys = []
+        browser.get(course[1])
+        topics = browser.find_elements_by_css_selector('.topics>li')
+        for topic in topics:
+            activity_list = []
+            for activity in topic.find_elements_by_css_selector('.content>.section.img-text>.activity'):
+                if (len(activity.find_elements_by_class_name('contentwithoutlink')) > 0):
+                    continue
+                current_activity = activity.find_element_by_css_selector('.activityinstance')
+                activity_list.append([current_activity.find_element_by_css_selector('.instancename').text,
+                                      current_activity.find_element_by_css_selector(
+                                          '.activityinstance>a').get_attribute('href')])
+
+            activitys.append([topic.find_element_by_css_selector('.sectionname>span>a').get_attribute('innerHTML'),
+                              activity_list])
+        data.append([course[0], activitys])
 
 
 def getCookies(browser):
@@ -30,7 +53,18 @@ def getSesskey(browser):
     return browser.execute_script("return M.cfg['sesskey']")
 
 
-def getRecentEvents(sesskey, mcookies):
-    postData = [{"index":0,"methodname":"core_calendar_get_action_events_by_timesort","args":{"limitnum":6,"timesortfrom":1619452800,"timesortto":1620057600,"limittononsuspendedevents":True}}]
-    link = "https://moodle2021.vtc.edu.hk/lib/ajax/service.php?sesskey="+sesskey+"&info=core_calendar_get_action_events_by_timesort"
+def fetchRecentEvents(sesskey, mcookies, timesortfrom,timesortto):
+    postData = [{"index": 0, "methodname": "core_calendar_get_action_events_by_timesort",
+                 "args": {"limitnum": 26, "timesortfrom": timesortfrom, "timesortto": timesortto,
+                          "limittononsuspendedevents": True}}]
+    link = os.getenv("moodle_host")+"/lib/ajax/service.php?sesskey=" + sesskey + "&info=core_calendar_get_action_events_by_timesort"
     r = requests.post(link, verify=False, cookies=mcookies, data=json.dumps(postData))
+    return r
+
+
+def getRecentEvents(response):
+    response = json.loads(response.text)
+    events = response[0]['data']['events']
+    for i in events:
+        print(i)
+    return events
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
 from function import *
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import time
 
+
+
 browser = webdriver.Chrome("chromedriver.exe")
-browser.get("https://moodle2021.vtc.edu.hk/")
+browser.get(os.getenv("moodle_host")+"/my")
 input()
-mycourses = browser.find_element_by_class_name('mycourses').find_elements_by_tag_name('li')
-data = []
-
-courses = grab_courses_list(mycourses)
-
-for course in courses:
-    activitys = []
-    browser.get(course[1])
-    topics = browser.find_elements_by_css_selector('.topics>li')
-    for topic in topics:
-        activity_list = []
-        for activity in topic.find_elements_by_css_selector('.content>.section.img-text>.activity'):
-            if (len(activity.find_elements_by_class_name('contentwithoutlink')) > 0):
-                continue
-            current_activity = activity.find_element_by_css_selector('.activityinstance')
-            activity_list.append([current_activity.find_element_by_css_selector('.instancename').text,
-                                  current_activity.find_element_by_css_selector('.activityinstance>a').get_attribute('href')])
-
-        activitys.append([topic.find_element_by_css_selector('.sectionname>span>a').get_attribute('innerHTML'),
-                          activity_list])
-    data.append([course[0], activitys])
-    #browser.implicitly_wait(1)
-print(data)
+timesortfrom = 1619452800
+timesortto = 1620057600
+data = getRecentEvents(fetchRecentEvents(getSesskey(browser), getCookies(browser), timesortfrom, timesortto))
 browser.quit()
-
